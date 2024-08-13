ec1ec3bbbb154b870ea63d33e216f0fa8413fb33
Kunal Zende <kunalzende17@gmail.com>
2024-08-13 23:46:36 +0530

    Create views folder to show listing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -13,11 +14,19 @@ main()
 
 async function main() {
     await mongoose.connect(MONGO_URL);
-    }
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.send("Hi I'm root");
 });
+
+app.get("/listing", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+})
 
 app.get("/testListing", async (req, res) => {
     // let sampleListing = new Listing({
@@ -26,10 +35,10 @@ app.get("/testListing", async (req, res) => {
     //     price: 12000,
     //     loaction: "Calangute, Goa",
     //     country: "India"
-    // });
-         await sampleListing.save();
-         console.log("Sample was saved");
-         res.send("Successful testing");
+    // // });
+    //      await sampleListing.save();
+    //      console.log("Sample was saved");
+    //      res.send("Successful testing");
 });
 
 app.listen(8080, () => {
